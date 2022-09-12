2e7ad13654e72c7b52be5163fb6ce064e2a5e5de
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-09-13 00:00:38 +0200

    implement half of the test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import Domain.Reiziger;
 import Domain.ReizigerDAO;
 import Domain.ReizigerDAOPsql;
 
 import java.sql.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -21,11 +23,42 @@ public class Main {
 //                System.out.println("#" + counter + ": " + myRs.getString("achternaam") + ", { " + myRs.getString("geboortedatum") + " }");
 //            }
             ReizigerDAOPsql daopsql = new ReizigerDAOPsql(mijnConn);
+
             System.out.println(daopsql.findAll());
+            testReizigerDAO(daopsql);
             mijnConn.close();
         } catch (Exception exc) {
             exc.printStackTrace();
         }
+
+    }
+    /**
+     * P2. Reiziger DAO: persistentie van een klasse
+     *
+     * Deze methode test de CRUD-functionaliteit van de Reiziger DAO
+     *
+     * @throws SQLException
+     */
+    private static void testReizigerDAO(ReizigerDAO rdao) throws SQLException {
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        System.out.print("[Test] Eerst " + ((List<?>) reizigers).size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
     }
 }
 
