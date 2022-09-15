b3a483a9a0aa71f86549f1c52099a95308af2c42
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-09-15 22:12:06 +0200

    AdresDAOPsql.java compleet!

--- src/DAOPsql/AdresDAOPsql.java
+++ src/DAOPsql/AdresDAOPsql.java
@@ -1,4 +1,125 @@
 package DAOPsql;
 
-public class AdresDAOPsql {
+import DAO.AdresDAO;
+import domain.Adres;
+import domain.Reiziger;
+
+import java.sql.*;
+
+public class AdresDAOPsql implements AdresDAO {
+    private Connection localConn;
+    private Statement myStatement;
+
+    public AdresDAOPsql(Connection conn) throws SQLException {
+        // 1. Connect met de database
+        localConn = conn;
+        // 2. Creeer een statement
+        Statement myStatement = localConn.createStatement();
+    }
+
+    public boolean save(Adres adres) {
+        String query = "INSERT INTO adres (adres_id, postcode, huisnummer, straat, woonplaats) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        try {
+
+            // PreparedStatement BRON: https://stackoverflow.com/questions/35554749/creating-a-prepared-statement-to-save-values-to-a-database
+            PreparedStatement ps = localConn.prepareStatement(query);
+            ps.setString(1, String.valueOf(adres.getId()));
+            ps.setString(2, adres.getPostcode());
+            ps.setString(3, adres.getHuisnummer());
+            ps.setString(4, adres.getStraat());
+            ps.setString(5, adres.getWoonplaats());
+
+            if (ps.executeUpdate() == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public boolean update(Adres adres) {
+        String query = "INSERT INTO adres (adres_id, postcode, huisnummer, straat, woonplaats) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        try {
+
+            // PreparedStatement BRON: https://stackoverflow.com/questions/35554749/creating-a-prepared-statement-to-save-values-to-a-database
+            PreparedStatement ps = localConn.prepareStatement(query);
+            ps.setString(1, String.valueOf(adres.getId()));
+            ps.setString(2, adres.getPostcode());
+            ps.setString(3, adres.getHuisnummer());
+            ps.setString(4, adres.getStraat());
+            ps.setString(5, adres.getWoonplaats());
+
+            if (ps.executeUpdate() == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param adres
+     * @return
+     */
+    @Override
+    public boolean delete(Adres adres) {
+        String query = "DELETE FROM adres WHERE adres_id = (adres_id) VALUES (?)";
+        try {
+            PreparedStatement ps = localConn.prepareStatement(query);
+            ps.setString(1, String.valueOf(adres.getId()));
+            ps.executeQuery();
+            return true;
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public Adres findById(int id) {
+        String query = "SELECT * FROM adres WHERE adres_id =  VALUES (?)";
+
+        try {
+            PreparedStatement ps = localConn.prepareStatement(query);
+            ps.setString(1, String.valueOf(id));
+
+            ResultSet myResultSet = ps.executeQuery();
+            return (Adres) myResultSet;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    /**
+     * @param reiziger
+     * @return
+     */
+    @Override
+    public Adres findByReiziger(Reiziger reiziger) {
+        String query = "SELECT * FROM adres WHERE reiziger_id =  VALUES (?)";
+
+        try {
+            PreparedStatement ps = localConn.prepareStatement(query);
+            ps.setString(1, String.valueOf(reiziger.getId()));
+
+            ResultSet myResultSet = ps.executeQuery();
+            return (Adres) myResultSet;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/domain/Adres.java
+++ src/domain/Adres.java
@@ -3,6 +3,10 @@ package domain;
 public class Adres {
     private int id;
     private String postcode;
+    private String huisnummer;
+    private String straat;
+    private String woonplaats;
+    private String reiziger_id;
 
     public String getPostcode() {
         return postcode;
@@ -10,6 +14,46 @@ public class Adres {
 
     public void setPostcode(String postcode) {
         this.postcode = postcode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHuisnummer() {
+        return huisnummer;
+    }
+
+    public void setHuisnummer(String huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public void setStraat(String straat) {
+        this.straat = straat;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public String getReiziger_id() {
+        return reiziger_id;
+    }
+
+    public void setReiziger_id(String reiziger_id) {
+        this.reiziger_id = reiziger_id;
     }
 
     @Override
